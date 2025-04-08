bc7de2e8c38f2242ee981e6ef29bb795fff32e74
Jessy_Pigman <Y66ask34@gmail.com>
2025-04-08 18:27:19 +0300

    Added server-to-database connection (added accounts table, worked with QtSql)

--- BoardDashTcpServer/mytcpserver.cpp
+++ BoardDashTcpServer/mytcpserver.cpp
@@ -13,8 +13,20 @@ MyTcpServer::~MyTcpServer()
 MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
     mTcpServer = new QTcpServer(this);
 
+    cache_database.setHostName("127.0.0.1");
+    cache_database.setDatabaseName("DashBoardMainDb");
+    cache_database.setUserName("Jessy_Pigman");
+    cache_database.setPassword("191030777");
+    cache_database.setPort(5432);   // ОБЯЗАТЕЛЬНО УКАЗАТЬ ЭТОТ ЖЕ ПОРТ ПРИ СОЗДАНИИ БАЗЫ ДАННЫХ
+
     connect(mTcpServer, &QTcpServer::newConnection,
             this, &MyTcpServer::slotNewConnection);
+
+    if (!cache_database.open()) {
+        qDebug() << "Failed to connect to database:" << cache_database.lastError().text();
+    } else {
+        qDebug() << "database connected";
+    }
 
     if(!mTcpServer->listen(QHostAddress::Any, 808)){
         qDebug() << "server is not started";
@@ -22,7 +34,11 @@ MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
         //server_status=1;
         qDebug() << "server is started";
     }
+
+
 }
+
+
 
 void MyTcpServer::checkClientNextAction(QByteArray action)
 {
@@ -35,8 +51,46 @@ void MyTcpServer::checkClientNextAction(QByteArray action)
     if (action == "reg")
         regNewUser();
 
+
     else if (action == "auth")
         authExtUser();
+}
+
+bool MyTcpServer::regNewUser()
+{
+    /**********
+     *  Механизм работы: Клиент отправляет запрос на регистрацию, и после получения ответа отправляет регистрационные данные,
+     *  сервер проверяет наличие данных, заносит данные в БД, если требуется, или возвращает ошибку.
+    **********/
+
+
+
+    return true;
+}
+
+bool MyTcpServer::authExtUser()
+{
+    /**********
+     *  Механизм работы: Клиент отправляет запрос на вход, и после получения ответа отправляет регистрационные данные,
+     *  сервер проверяет наличие данных, вызывает метод передачи данных на доску к клиенту или возвращает ошибку.
+    **********/
+    return true;
+}
+
+void MyTcpServer::dbquest(QString request)
+{
+    QString result;
+    if (main_db_query.exec(request))
+    {
+        while(main_db_query.next())
+        {
+            for (int counter = 0; counter < 5; counter++)
+            {
+                result = main_db_query.value(counter).toString();
+                qDebug() << result;
+            }
+        }
+    }
 }
 
 void MyTcpServer::slotNewConnection(){
---
+++ Database/account_table_create_script.sql
@@ -0,0 +1,13 @@
+--create table Accounts (
+
+--	"id" serial primary key not null,
+--	"account_name" varchar(20),
+--	"account_password" varchar(20),
+--	"account_email" varchar(30),
+--	"ip_address" varchar(15)
+--)
+
+--select * from Accounts;
+
+--insert into Accounts (account_name, account_password, account_email, ip_address) values ('Y66ask34', 'Y66ask191030777', 'Y66ask34@gmail.com', '127.0.0.1');
+
