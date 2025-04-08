1b800f4476430251faeebdc033a7d1e757f522fb
Jessy_Pigman <Y66ask34@gmail.com>
2025-04-08 22:18:21 +0300

    Ui_maintenance (Added UI, without functionality)

--- BoardDashClient/main.cpp
+++ BoardDashClient/main.cpp
@@ -2,10 +2,12 @@
 
 #include <QApplication>
 #include <QTcpSocket>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
     w.show();
 
